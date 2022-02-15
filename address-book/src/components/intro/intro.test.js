994a23c1f95c7303a6befd397926c03680f921cd
Ryan Grimes <75947453+RPGrimes@users.noreply.github.com>
2022-02-15 20:13:46 +0000

    Delete intro.test.js

--- address-book/src/components/intro/intro.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import {render, screen, fireEvent, act} from '@testing-library/react';
-import App from '../../App';
-
-it("renders App component correctly", () => { 
-  const { getByText } = render(<App />);
-  expect(getByText(/Address Book/i)).toBeInTheDocument();
-});
