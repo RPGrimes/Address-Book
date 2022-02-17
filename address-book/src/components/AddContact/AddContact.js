fc890950415c3095952186489c1ffd42c0dbfc54
Ryan Grimes <ryanpaulgrimes@gmail.com>
2022-02-17 17:46:03 +0100

    Number validation added. MVP achieved

--- address-book/src/components/AddContact/AddContact.js
+++ address-book/src/components/AddContact/AddContact.js
@@ -1,10 +1,9 @@
-import "./addContact.scss"
+import "./addContact.scss";
 import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const AddContact = (props) => {
-
   let navigate = useNavigate();
 
   //------------------------
@@ -21,82 +20,136 @@ const AddContact = (props) => {
     setContactNumber(e.target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleChange = (e) => {
     e.preventDefault();
-    
-    if(checkValidName() === false){
-      return alert("Invalid name entered. Names must be comprised of letters or spaces ONLY. Please try again!")
+
+    if (checkValidName() === false) {
+      return alert(
+        "Invalid name entered. Names must be comprised of letters or spaces ONLY. Please try again!"
+      );
     }
-  //  validation functions to go in here below to be reviewed upon submit
 
-    const oldState = props.addressList
-    const newContact = {name:contactName, number:contactNumber}
-    const newState = [...oldState, newContact] //Old state is a list of objects in an array, in order to make a new array of all the old objects plus the new contact. 
-    
+    if (checkValidNumber() === false) {
+      return alert(
+        'Invalid number entered. Number must contain only numerical digits, "+" or parentheses. Please try again!'
+      );
+    }
+
+    if (checkValidNumberStart() === false) {
+      return alert(
+        'Invalid number entered. Number must start with either "+" or "0". If it starts with a + the second digit may NOT be "0 Please try again!'
+      );
+    }
+
+    if (checkNumberOfBrackets() === false) {
+      return alert(
+        "Invalid number entered. Number must have either 0 or 1 complete set of parentheses. Please try again!"
+      );
+    }
+
+    const oldState = props.addressList;
+    const newContact = { name: contactName, number: contactNumber };
+    const newState = [...oldState, newContact]; //Old state is a list of objects in an array, in order to make a new array of all the old objects plus the new contact.
+
     props.setAddressList(newState);
-  }
+  };
 
-  //ADD VALIDATION FUNCTIONS HERE
+  //------------------------
+  // VALIDATION FUNCTIONS
+  //------------------------
   const checkValidName = () => {
-    return /^[a-zA-Z\s]*$/.test(contactName)
-  }
+    return /^[a-zA-Z\s]*$/.test(contactName);
+  };
 
-  console.log(checkValidName())
+  const checkValidNumberStart = (number) => {
+    const contactNumberArray = contactNumber.split("");
+    if (contactNumberArray[0] !== "0" && contactNumberArray[0] !== "+") {
+      return false;
+    }
+    if (contactNumberArray[0] === "+" && contactNumberArray[1] === "0") {
+      return false;
+    }
+  };
 
-  const checkValidNumber = (contactNumber) => {
+  const checkNumberOfBrackets = (number) => {
+    let string = "()";
+    const stringArr = string.split("");
 
-  }
+    let openParCount = 0;
+    let closeParCount = 0;
+
+    const contactNumberArray = contactNumber.split("");
+
+    contactNumberArray.forEach((element) =>
+      element === stringArr[0] ? openParCount++ : null
+    );
+    contactNumberArray.forEach((element) =>
+      element === stringArr[1] ? closeParCount++ : null
+    );
+
+    if (openParCount !== closeParCount) {
+      return false;
+    } else if (openParCount > 1) {
+      return openParCount > 1 ? false : null;
+    }
+  };
+
+  const checkValidNumber = () => {
+    return /^[0-9\(\)\+]*$/.test(contactNumber);
+  };
+
+  //------------------------
+  // MAIN BODY OF COMPONENT RETURN
+  //------------------------
 
   return (
-    <div className="addContact" id="addContact">
-        <div>
-          <h3>Enter a contact name and telephone number</h3>
-          <div className='formContainer' onSubmit={handleSubmit}>
-            <form>
+    <div className='addContact' id='addContact'>
+      <div>
+        <h3>Enter a contact name and telephone number</h3>
+        <div className='formContainer' onSubmit={handleChange}>
+          <form>
             <label>
-              Name:  
+              Name:
               <input
-                type="text"
-                placeholder="Contact Name"
-                name="contactName"
+                type='text'
+                placeholder='Contact Name'
+                name='contactName'
                 value={contactName}
-                onChange={handleContactName}
-                ></input>
-            </label> <br />
+                onChange={handleContactName}></input>
+            </label>{" "}
+            <br />
             <label>
               Tel:
               <input
-                type="text"
-                placeholder="Contact Number"
-                name="contactNumber"
+                type='text'
+                placeholder='Contact Number'
+                name='contactNumber'
                 value={contactNumber}
-                onChange={handleContactNumber}
-                ></input>
-            </label> <br  />
-            <button type="submit">Submit</button>
-            </form>
+                onChange={handleContactNumber}></input>
+            </label>{" "}
+            <br />
+            <button type='submit'>Submit</button>
+          </form>
         </div>
       </div>
       <button
-        className="btn-start"
-        type="submit"
+        className='btn-start'
+        type='submit'
         onClick={() => {
           navigate("list");
-        }}
-      >
+        }}>
         Full Address Book
       </button>
       <button
-          className="btn-start"
-          type="submit"
-          onClick={() => {
-            navigate("/search");
-          }}
-        >
-          Search Contacts
-        </button>
+        className='btn-start'
+        type='submit'
+        onClick={() => {
+          navigate("/search");
+        }}>
+        Search Contacts
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default AddContact;
